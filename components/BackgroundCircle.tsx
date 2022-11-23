014bed53ef75f5abba094d9c2fedfdc189ed8655
Zaki Samson <zakisamson@gmail.com>
2022-11-23 19:09:16 +0700

    perfecting the pinging circle

--- components/BackgroundCircle.tsx
+++ components/BackgroundCircle.tsx
@@ -18,7 +18,7 @@ function BackgroundCircle({}: Props) {
         duration: 2.5
     }}
     className='relative flex justify-center items-center'>
-        {/* <div className='absolute border border-[#333333] rounded-full h-[200px] w-[200px] mt-52 ' /> */}
+        <div className='absolute border border-[#333333]/50 rounded-full h-[200px] w-[200px] mt-52 animate-ping' />
         <div className='absolute border border-[#333333] rounded-full h-[300px] w-[300px] mt-52'/>
         <div className='absolute border border-[#333333] rounded-full h-[500px] w-[500px] mt-52' />
         <div className='border border-[#e28743] rounded-full opacity-20 h-[650px] w-[650px] mt-52 absolute animate-pulse' />
