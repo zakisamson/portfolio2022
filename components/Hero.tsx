83a88f619fbdb2f7e2abd0b46f2d676b39605246
Zaki Samson <zakisamson@gmail.com>
2022-11-23 19:05:38 +0700

    finished hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,11 +1,46 @@
+import Image from 'next/image'
 import React from 'react'
+import { Cursor, useTypewriter } from "react-simple-typewriter"
+import BackgroundCircle from './BackgroundCircle'
+import profilePic from '../public/foto.jpeg'
+import Link from 'next/link'
 
 type Props = {}
 
-function Hero({}: Props) {
-  return (
-    <div>Hero</div>
-  )
+function Hero({ }: Props) {
+    const [text, count] = useTypewriter({
+        words: ['My name is Zaki Samson', 'Front End Developer', 'Game Enthusiast'],
+        loop: true,
+        delaySpeed: 1500
+    })
+
+    return (
+        <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
+            <BackgroundCircle />
+            <img className='relative rounded-full mx-auto h-32 2-32 object-cover' src="https://media-exp1.licdn.com/dms/image/C4D03AQFRVdDrTDV7Tg/profile-displayphoto-shrink_100_100/0/1654474669288?e=1674691200&v=beta&t=N8YPSVPRSAeLSJTBnRcvclaZMuDqTQOLTAtW-aKS5tg" alt="Zaki's profile picture" />
+            <div className='z-20'>
+                <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>Web Developer</h2>
+                <h1 className='text-5xl lg:text-6xl font-semibold px-10'>
+                    <span className='mr-3'>{text}</span>
+                    <Cursor cursorColor="#e28743" />
+                </h1>
+                <div className='pt-5'>
+                    <Link href="#about">
+                        <button className='heroButton'>About</button>
+                    </Link>
+                    <Link href="#experience">
+                        <button className='heroButton'>Experience</button>
+                    </Link>
+                    <Link href="#skills">
+                        <button className='heroButton'>Skills</button>
+                    </Link>
+                    <Link href="#projects">
+                        <button className='heroButton'>Projects</button>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Hero
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
         <title>Zaki's Portfolio</title>
         <meta name="description" content="Made with love" />
@@ -13,19 +13,29 @@ export default function Home() {
 
       <Header />
       {/* Hero */}
-      <section id="hero">
+      <section id="hero" className='snap-center'>
         <Hero />
       </section>
       {/* About */}
-
+      <section id="about" className='snap-center'>
+        <Hero />
+      </section>
       {/* Experience */}
-
+      <section id="experience" className='snap-center'>
+        <Hero />
+      </section>
       {/* Skills */}
-
+      <section id="skills" className='snap-center'>
+        <Hero />
+      </section>
       {/* Projects */}
-
+      <section id="projects" className='snap-center'>
+        <Hero />
+      </section>
       {/* Contact Me */}
-
+      <section id="contact" className='snap-center'>
+        <Hero />
+      </section>
     </div>
   )
 }
